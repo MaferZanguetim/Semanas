122c658f41a1847f30a313d2d8b2ed00162c5c14
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2022-06-06 17:00:19 +0000

    Semanas

---
+++ Semanas.java
@@ -0,0 +1,11 @@
+public class Semanas{
+    public static void main(String[] args) {
+        int segundaSemana=5;
+        int terceiraSemana=2;
+        int quartaSemana=7;
+
+        System.out.println("Segunda Semana = " + segundaSemana);
+        System.out.println("Terceira Semana = "+ terceiraSemana);
+        System.out.println("Quarta Semana = "+ quartaSemana);
+    }
+}
